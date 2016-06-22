bba70303b48d7369481dd76ae4f844aefc3ded91
Neil Kalman <neilk@redkix.com>
2016-06-22 15:03:24 +0300

    add localhost and my own environment

--- app/src/helpers/helpers.js
+++ app/src/helpers/helpers.js
@@ -24,7 +24,9 @@ function linkIsInternal(currentUrl, newUrl) {
       newUrl.indexOf('int.redkix.com') !== -1 ||
       newUrl.indexOf('staging.redkix.com') !== -1 ||
       newUrl.indexOf('gmail.redkix.com') !== -1 ||
-      newUrl.indexOf('beta.redkix.com') !== -1;
+      newUrl.indexOf('beta.redkix.com') !== -1 ||
+      newUrl.indexOf('10.0.0.108:9000') !== -1 ||
+      newUrl.indexOf('0.0.0.0:9000') !== -1;
 }
 
 function getCssToInject() {
